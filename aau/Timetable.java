172e18945168724f3a48e8e2c30009a9c4ae46b4
Jens Tinggaard <mail@t1ng.dk>
2023-10-08 22:51:27 +0200

    Added Reservation class

--- aau/Timetable.java
+++ aau/Timetable.java
@@ -1,5 +1,18 @@
 package aau;
 
-public class Timetable {
 
+public class Timetable {
+    private Room room;
+
+    // consturctors
+
+    public Timetable(Room room) {
+        this.room = room;
+    }
+
+    // getters
+
+    public Room getRoom() {
+        return this.room;
+    }
 }
